aef0b0f682ea827da10ff79175c447853d3c117f
Maxim <maximalimovdeveloper@gmail.com>
2024-08-07 19:03:57 +0300

    First step

--- src/main/java/com/maxim/Simulation.java
+++ src/main/java/com/maxim/Simulation.java
@@ -1,33 +1,20 @@
 package com.maxim;
 
-import com.maxim.actions.Actions;
-import com.maxim.map.Map;
-import lombok.Getter;
-
-@Getter
 public class Simulation {
-    private final Map map;
-    private int stepsCount;
 
     public static void main(String[] args) {
-        Simulation simulation = new Simulation();
-        Actions.initActions(simulation.map);
+        //Actions.initActions();
 
-        simulation.getMap().printMatrix();
     }
-
-    public Simulation() {
-        this.map = new Map();
-    }
-
-    public static void initGame() {
-    }
-
     public void nextTurn() {
 
+
+    }
+    public void startSimulation() {
+
     }
 
-    public void renderMap() {
+    public void pauseSimulation(){
 
     }
 }
